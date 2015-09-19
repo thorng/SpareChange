4efef67ae9436e480cd6dbec4e06c2ccdad71a3b
Timothy Horng <thorng@asu.edu>
2015-09-19 15:45:47 -0400

    first commit

---
+++ SpareChange/DonateViewController.swift
@@ -0,0 +1,42 @@
+//
+//  DonateViewController.swift
+//  SpareChange
+//
+//  Created by Timothy Horng on 9/19/15.
+//  Copyright © 2015 Timothy Horng. All rights reserved.
+//
+
+import UIKit
+
+class DonateViewController: UIViewController {
+
+    @IBOutlet weak var donateButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func donateButtonPressed(sender: UIButton) {
+        
+        print("button pressed :3")
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
