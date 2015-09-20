30ee40859eb821cc9af7f03133196cd5027f8cbd
Timothy Horng <thorng@asu.edu>
2015-09-20 07:28:12 -0400

    much much changes

--- SpareChange/DonateViewController.swift
+++ SpareChange/DonateViewController.swift
@@ -216,7 +216,7 @@ class DonateViewController: UIViewController, CountdownTimerDelegate, UIScrollVi
     }
     
     func fadeOut() {
-        UIView.animateWithDuration(1.0, delay: 0.0, options: UIViewAnimationOptions.CurveEaseOut, animations: {
+        UIView.animateWithDuration(0.5, delay: 0.0, options: UIViewAnimationOptions.CurveEaseOut, animations: {
             self.damageLabel.alpha = 0.0
             }, completion: nil)
     }
